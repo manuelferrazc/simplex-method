54bb4d6dc908b48fb34ffb92c39b763e11b5100f
Manueljunio <manueljunioferraz@hotmail.com>
2024-12-31 10:26:47 -0300

    initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import util
+import numpy as np
+import argparse
+
+def zero(x,e=1e-8):
+    return 0.0 if abs(x)<e else x
+
+
+
+
+
+
+
+
+
+def main():
+    print('ababa')
+
+if __name__ == '__main__': main()
---
+++ util.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+def makeMatrixFullRank(A):
+    ''' Esta função recebe uma matriz, que pode ser em numpy,
+        e retorna dois argumentos:
+          - A matriz com linhas eliminadas
+          - Uma lista indicando quais linhas foram eliminadas.
+    '''
+    if np.linalg.matrix_rank(A) == A.shape[0]: return A, []
+    row = 1
+    rowsEliminated = []
+    counter = 0
+    while 1:
+        counter += 1
+        B = A[0:(row+1), :]
+        C = np.linalg.qr(B.T)[1]
+        C[np.isclose(C, 0)] = 0
+        if not np.any(C[row, :]):
+            rowsEliminated.append(counter)
+            A = np.delete(A, (row), axis=0)
+        else:
+            row += 1
+        # end if
+        if row >= A.shape[0]: break
+    # end for
+    return A, rowsEliminated
+# end makeMatrixFullRank
+
